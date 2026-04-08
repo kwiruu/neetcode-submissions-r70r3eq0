bb1270d36c2f1ad7ddf46f88f45e6e48f5127b43
kwiruu <keiru277@gmail.com>
2026-04-08 23:08:33 +0800

    Add: baseball-game - submission-17

---
+++ Data Structures & Algorithms/baseball-game/submission-17.py
@@ -0,0 +1,16 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        temp = []
+        for i in range(len(operations)):
+            match operations[i]:
+                case 'D':
+                    temp.append(int(temp[-1])*2)
+                case '+':
+                    temp.append(int(temp[-2])+int(temp[-1]))
+                case 'C':
+                    temp.pop()
+                case _:
+                    temp.append(int(operations[i]))
+        return sum(temp)
+
+            
